a9c66c029a05149bfaeb442a25b623483dfcbfee
Ivy Do <ivy.do2011@gmail.com>
2021-12-27 07:20:24 -0500

    added README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateReadme(answers) {
+  return `
+<h1 align="center">${answers.projectTitle}</h1>
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+## Description
+🔍 ${answers.description}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-`;
+## Installation
+💾 ${answers.installation}
+
+## Usage
+💻 ${answers.usage}
+
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+
+## Contributing
+👪 ${answers.contributing}
+
+## Tests
+✏️ ${answers.tests}
+
+## Questions
+✋ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+✉️ Email me with any questions: ${answers.email}<br /><br />
+
+_This README was generated with ❤️ by [README-generator](https://github.com/ivydo/README-Generator-) 🔥🔥🔥_
+  `;
 }
+module.exports = generateReadme;
 
-module.exports = generateMarkdown;
